8fc2c9c3abbf46988937c1ee17863097006278ed
Chetna Malhotra <chetna.malhotra@wavelabs.ai>
2020-08-22 14:01:16 +0530

    visual changes

--- client/src/components/balance.js
+++ client/src/components/balance.js
@@ -5,11 +5,11 @@ export const Balance = () => {
     const {transactions}=useContext(GlobalContext);
     const amounts = transactions.map(transaction => transaction.amount);
     console.log(amounts);
-    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(0);
     return (
         <div>
             <h4>Your Balance</h4>
-    <h1 id="balance">${total}</h1>
+    <h1 id="balance">Rs. {total}</h1>
         </div>
     )
 }
--- client/src/components/transaction.js
+++ client/src/components/transaction.js
@@ -9,7 +9,7 @@ export const Transaction = ({transaction}) => {
                <li className={transaction.amount<0?"minus":"plus"}>
                {transaction.text} 
                     <span>
-                   {sign} $ {Math.abs(transaction.amount)}
+                   {sign} Rs. {Math.abs(transaction.amount)}
                    
                    </span>
                <button className="delete-btn" onClick={()=>deletetransaction(transaction._id)}>
